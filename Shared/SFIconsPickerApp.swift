103998f6839f44067970e03d24a0a028506383d0
thoamsy <c6swift@gmail.com>
2020-11-21 11:19:13 +0800

    Initial Commit

---
+++ Shared/SFIconsPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFIconsPickerApp.swift
+//  Shared
+//
+//  Created by yk on 2020/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SFIconsPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

67540b555f01c5916d56274ac32a9bf365a874c6
thoamsy <c6swift@gmail.com>
2020-11-23 23:18:02 +0800

    添加 color picker，虽然不管用……

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,33 +6,42 @@ import Combine
 struct ContentView: View {
   let icons: [String]
   let title: String
+  @State private var tintColor: Color = .primary
   @State var selectedIndex = -1
 
   var columns: [GridItem] {
-    Array(repeating: .init(.adaptive(minimum: 80)), count: 5)
+    Array(repeating: .init(.adaptive(minimum: 100)), count: 4)
   }
 
   var body: some View {
+    print(tintColor)
     return NavigationView {
       ScrollView {
         LazyVGrid(columns: columns) {
           ForEach(Array(icons.enumerated()), id: \.1) { index, name in
-            Label(name, systemImage: name)
-              .labelStyle(IconOnlyLabelStyle())
-              .onTapGesture {
-                selectedIndex = index
-              }
-              .frame(width: 64, height: 64)
-              .cornerRadius(4)
-              .padding((80 - 64)/2)
-              .border(Color(UIColor.separator))
-              .background(index == selectedIndex ? Color(UIColor.link) : Color(UIColor.secondarySystemGroupedBackground))
+            VStack {
+              Image(systemName: name)
+                .renderingMode(.template)
+                .foregroundColor(tintColor)
+                .onTapGesture {
+                  selectedIndex = index
+                }
+                .frame(width: 80, height: 80)
+                .cornerRadius(4)
+                .border(Color(UIColor.separator))
+                .padding([.bottom], 4)
+
+              Text(name).foregroundColor(.primary).font(.footnote)
+            }
+            .frame(height: 120)
+            //              .background(index == selectedIndex ? Color(UIColor.link) : Color(UIColor.secondarySystemGroupedBackground))
           }
           .font(.title2)
           .padding()
         }
       }
       .navigationTitle(title)
+      .navigationBarItems(trailing: ColorPicker("", selection: $tintColor, supportsOpacity: false))
     }
   }
 }
